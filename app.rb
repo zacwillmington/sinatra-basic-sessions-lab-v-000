e276e69bfd977dce17e7f02b9ebb50ff4073540f
Zac Willmington <zac.willmington@gmail.com>
2018-05-14 17:49:25 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    configure do
+        enable :sessions
+        set :sessions_secret, "you shall never know"
+    end
+
+    get '/' do
+        erb :index
+    end
+
+    post '/checkout' do
+        session["item"] = params[:item]
+        @session = session
+        erb :checkout
+    end
 end
