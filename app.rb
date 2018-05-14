585012261fa36bbf6dd867f613c6481049ff7f2e
Zac Willmington <zac.willmington@gmail.com>
2018-05-14 17:34:25 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,8 +1,10 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
     configure do
-    enable :sessions
-    set :sessions_secret, "you shall never know"
+        enable :sessions
+        set :sessions_secret, "you shall never know"
+    end
     
 end
